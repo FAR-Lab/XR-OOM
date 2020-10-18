50416a3516ebc6da7456e0981b7e688a85b47a32
DavidGoedicke <2676840+DavidGoedicke@users.noreply.github.com>
2020-10-18 17:33:39 -0400

    Back-up of whiteboard tracking.
    Remaining issues:
    - Lag needs to be replaced by saving marker location in stead of re-calculating every frame.
    - Folder structure needs cleaning up.
    - Multi-marker tracking needs to be implemented in order to stabilize location.

---
+++ Assets/Scripts/VarjoXRInit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VarjoXRInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Varjo.XR.VarjoMixedReality.StartRender();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
