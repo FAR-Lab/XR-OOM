7564dc40fe08d7bfaa26b358ab87643191ebd1ab
John O'Neill <john@triangledivision.com>
2020-11-05 13:54:43 -0500

    Update to show MarkerManager
    Added a marker manager class to give you an example how to use simple lerping to approximate and smooth marker positions.  Also has the option to keep a marker object visible even when the detection doesn't see it (in case you update the hierarchy and add a visible object attached to the position.)

---
+++ Assets/Scripts/MarkerTrackingOrigin.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Varjo.XR;
+
+public class MarkerTrackingOrigin : MonoBehaviour {
+    public static MarkerTrackingOrigin Instance { get; private set; } = null;
+    public float lerpRate = 2.0f;
+    public bool disableWhenMarkerNotVisible = false;
+
+    public delegate void MarkersActive();
+    public static MarkersActive OnMarkersActiveEvent;
+
+    public delegate void MarkersDisabled();
+    public static MarkersDisabled OnMarkersDisabledEvent;
+
+    public GameObject rootMarker;
+
+    private Vector3 _averagePosition;
+    private Quaternion _averageRotation;
+    private List<VarjoMarker> _markers = new List<VarjoMarker>();
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public void Update() {
+        if (VarjoEventManager.Instance.GetButtonDown(0) && VarjoMixedReality.IsMRAvailable()) {
+            bool enable = !VarjoMarkers.IsVarjoMarkersEnabled();
+            VarjoMarkers.EnableVarjoMarkers(enable);
+
+            if (enable)
+                OnMarkersActiveEvent.Invoke();
+            else
+                OnMarkersDisabledEvent();
+
+            if (VarjoMarkers.GetVarjoMarkerCount() > 0) {
+                rootMarker.SetActive(true);
+            }
+        }
+
+
+        if (!VarjoMarkers.IsVarjoMarkersEnabled()) {
+            rootMarker.SetActive(!disableWhenMarkerNotVisible);
+            return;
+        }
+
+        // This simply averages the marker positions of any that are detected
+        _markers.Clear();
+        VarjoMarkers.GetVarjoMarkers(out _markers);
+
+        if (_markers.Count > 0) {
+            _averagePosition = Vector3.zero;
+            _averageRotation = Quaternion.identity;
+
+            foreach(var marker in _markers) {
+                _averagePosition += marker.pose.position;
+                _averageRotation = marker.pose.rotation;
+            }
+            _averagePosition /= _markers.Count;
+
+            Vector3 euler = _averageRotation.eulerAngles;
+
+            transform.position = Vector3.Lerp(transform.position, _averagePosition, Time.deltaTime * lerpRate);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, euler.y, 0f), Time.deltaTime * lerpRate);
+        }
+    }
+}
