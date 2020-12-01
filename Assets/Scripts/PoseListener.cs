759ba50cd16ac57d57b7fbf76d2471dcf440be4e
DavidGoedicke <2676840+DavidGoedicke@users.noreply.github.com>
2020-12-01 15:35:26 -0500

    Implemented PoseCube to follow ZedMini pose via ROS
    Co-Authored-By: Alexandra Bremers <24699361+bremers@users.noreply.github.com>

---
+++ Assets/Scripts/PoseListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using RosPose = RosMessageTypes.Geometry.PoseStamped;
+
+public class PoseListener : MonoBehaviour
+{
+    public ROSConnection ros;
+    public GameObject cube;
+
+    void Start()
+    {
+        ros.Subscribe<RosPose>("/zedm/zed_node/pose", PoseChange);
+    }
+
+    void PoseChange(RosPose poseMessage)
+    {
+        Debug.Log(poseMessage);
+        cube.transform.position = new Vector3(
+            (float)poseMessage.pose.position.x,
+            (float)poseMessage.pose.position.y,
+            (float)poseMessage.pose.position.z
+            );
+    }
+}
---
+++ Assets/Scripts/RosSubscriberExample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using RosColor = RosMessageTypes.RoboticsDemo.UnityColor;
+
+public class RosSubscriberExample : MonoBehaviour
+{
+    public ROSConnection ros;
+    public GameObject cube;
+
+    void Start()
+    {
+        ros.Subscribe<RosColor>("thisisatest", ColorChange);
+    }
+
+    void ColorChange(RosColor colorMessage)
+    {
+        Debug.Log(colorMessage);
+        cube.GetComponent<Renderer>().material.color = new Color32((byte)colorMessage.r, (byte)colorMessage.g, (byte)colorMessage.b, (byte)colorMessage.a);
+    
+    }
+}
