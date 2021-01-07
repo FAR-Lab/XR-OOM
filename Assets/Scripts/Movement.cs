3f2b8ab3b1c8fd9a88fa836debc7effc3deef6c4
Alexandra Bremers <alexandrabremers@gmail.com>
2021-01-07 22:19:34 +0100

    Added Movement.cs for key input based object movement
    left tab + A/W/S/D/UpArrow/DownArrow = slow XYZ movement
    left tab + LeftArrow/RightArrow = slow left/right rotation
    left shift + A/W/S/D/UpArrow/DownArrow = fast XYZ movement
    left shift + LeftArrow/RightArrow = fast left/right rotation
    
    can be attached directly to traffic cones object

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+  
+public class Movement : MonoBehaviour  
+{  
+     
+    // Start is called before the first frame update  
+    void Start()  
+    {  
+          
+    }  
+  
+    // Update is called once per frame  
+    void Update()  
+    {  
+        // Slow movement
+        if (Input.GetKey(KeyCode.Tab))  {  
+            if (Input.GetKey(KeyCode.W))  
+            {  
+               this.transform.Translate(Vector3.forward * Time.deltaTime);  
+            }
+         
+            if (Input.GetKey(KeyCode.S))  
+            {  
+                this.transform.Translate(Vector3.back * Time.deltaTime);  
+            } 
+
+            if (Input.GetKey(KeyCode.A))  
+            {  
+               this.transform.Translate(Vector3.left * Time.deltaTime);  
+            }
+         
+            if (Input.GetKey(KeyCode.D))  
+            {  
+                this.transform.Translate(Vector3.right * Time.deltaTime);  
+            }   
+
+            if (Input.GetKey(KeyCode.UpArrow))  
+            {  
+               this.transform.Translate(Vector3.up * Time.deltaTime);  
+            }
+         
+            if (Input.GetKey(KeyCode.DownArrow))  
+            {  
+                this.transform.Translate(Vector3.down * Time.deltaTime);  
+            }   
+         
+            if (Input.GetKey(KeyCode.LeftArrow))  
+            {  
+                this.transform.Rotate(Vector3.up, -2);  
+            }  
+        
+            if (Input.GetKey(KeyCode.RightArrow))  
+            {  
+                this.transform.Rotate(Vector3.up, 2);  
+            }  
+        }
+
+        // Fast movement
+        if (Input.GetKey(KeyCode.LeftShift))  {  
+            if (Input.GetKey(KeyCode.W))  
+            {  
+               this.transform.Translate(Vector3.forward * 5*Time.deltaTime);  
+            }
+         
+            if (Input.GetKey(KeyCode.S))  
+            {  
+                this.transform.Translate(Vector3.back * 5*Time.deltaTime);  
+            } 
+
+            if (Input.GetKey(KeyCode.A))  
+            {  
+               this.transform.Translate(Vector3.left * 5*Time.deltaTime);  
+            }
+         
+            if (Input.GetKey(KeyCode.D))  
+            {  
+                this.transform.Translate(Vector3.right * 5*Time.deltaTime);  
+            }   
+
+            if (Input.GetKey(KeyCode.UpArrow))  
+            {  
+               this.transform.Translate(Vector3.up * 5*Time.deltaTime);  
+            }
+         
+            if (Input.GetKey(KeyCode.DownArrow))  
+            {  
+                this.transform.Translate(Vector3.down * 5*Time.deltaTime);  
+            }   
+         
+            if (Input.GetKey(KeyCode.LeftArrow))  
+            {  
+                this.transform.Rotate(Vector3.up, -10);  
+            }  
+        
+            if (Input.GetKey(KeyCode.RightArrow))  
+            {  
+                this.transform.Rotate(Vector3.up, 10);  
+            }  
+        }
+    }  
+} 
