6037221b56ba718beed37ba11ba124b0a61f8c9d
David Goedicke <2676840+DavidGoedicke@users.noreply.github.com>
2021-08-01 23:42:06 -0400

    Storing calibration results

---
+++ Assets/Scripts/Calibration/ConfigFileTester.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigFileTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Parent;
+    public Transform Child;
+
+    public bool load;
+    public bool store;
+
+    private ConfigFileLoading conf;
+    void Start()
+    {
+        conf = GetComponent<ConfigFileLoading>();
+        Child.parent = Parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (load)
+        {
+            load = false;
+           
+            conf.LoadLocalOffset(out Vector3 pos,out Quaternion rot);
+            Child.localPosition = pos;
+            Child.localRotation = rot;
+        }
+        else if (store)
+        {
+            store = false;
+           
+            conf.StoreLocalOffset(Child.localPosition,Child.localRotation );
+        }
+       
+    }
+}
